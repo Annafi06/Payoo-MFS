ab4c67658c1fbb848eea5df2dfeb0beb2006992b
Annafi06 <abdunnafi28@gmail.com>
2024-09-21 18:21:10 +0600

    handle add money event handler on the button click

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home added')
+// add money to the acount
+
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+});
