6a2eaa053e636e856752b937ddd258561aa22942
Annafi06 <abdunnafi28@gmail.com>
2024-09-21 14:38:39 +0600

    Handle Login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -1,7 +1,31 @@
-document.getElementById('button-login').addEventListener('click', function(event){
+// document.getElementById('button-login').addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log('login button e chaap maros ken halarpo');
+
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);
+
+//     if(phoneNumber === '01735205584' && pinNumber === '2403'){
+//         console.log('You are logged in');
+//     }
+//     else{
+//         alert('Kirre hacker bhai ar koto')
+//     }
+// })
+
+document.getElementById('button-login').addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('login button e chaap maros ken halarpo');
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-})
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+
+    if (phoneNumber === '01735205584' && pinNumber === '2403') {
+        console.log('You are logged in');
+        window.location.href = '/home.html'
+    }
+    else {
+        alert('Kirre hacker bhai ar koto')
+    }
+});
