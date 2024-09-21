d05851f95f836006d6008a28381c32fcd62ab614
Annafi06 <abdunnafi28@gmail.com>
2024-09-21 19:16:13 +0600

    Verify Pin Number and add money to the balance

--- js/home.js
+++ js/home.js
@@ -8,4 +8,21 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
 
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+
+    if (pinNumberInput === '1234') {
+        console.log('add money to your account');
+
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert('Failed to add money to your account. please try again!');
+    }
 });
