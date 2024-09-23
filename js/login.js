773dae9b96e671fabc2feaf292d2d504198dcfca
Annafi06 <abdunnafi28@gmail.com>
2024-09-23 14:12:11 +0600

    added toggle feature

--- js/login.js
+++ js/login.js
@@ -1,19 +1,3 @@
-// document.getElementById('button-login').addEventListener('click', function(event){
-//     event.preventDefault();
-//     console.log('login button e chaap maros ken halarpo');
-
-//     const phoneNumber = document.getElementById('phone-number').value;
-//     const pinNumber = document.getElementById('pin-number').value;
-//     console.log(phoneNumber, pinNumber);
-
-//     if(phoneNumber === '01735205584' && pinNumber === '2403'){
-//         console.log('You are logged in');
-//     }
-//     else{
-//         alert('Kirre hacker bhai ar koto')
-//     }
-// })
-
 document.getElementById('button-login').addEventListener('click', function (event) {
     event.preventDefault();
 
@@ -21,7 +5,7 @@ document.getElementById('button-login').addEventListener('click', function (even
     const pinNumber = document.getElementById('pin-number').value;
     // console.log(phoneNumber, pinNumber);
 
-    if (phoneNumber === '01735205584' && pinNumber === '2403') {
+    if (phoneNumber === '01735205584' && pinNumber === '1234') {
         console.log('You are logged in');
         window.location.href = '/home.html'
     }
